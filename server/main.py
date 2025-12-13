55c8dfd89f1d26470abb0521339ac255ef1fc663
V-Srivatsan <vatsan.programming@gmail.com>
2025-12-14 00:47:54 +0530

    Shifted news route and added leaderboard

--- server/main.py
+++ server/main.py
@@ -1,6 +1,4 @@
 import data.db as db
-from user import models as user_models
-from stock import models as stock_models
 db.sql.SQLModel.metadata.create_all(db.engine)
 
 
@@ -8,6 +6,7 @@ from fastapi import FastAPI, Depends, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from user.views import router as user_routes
 from stock.views import router as stock_routes
+from misc_views import router as misc_routes
 
 
 app = FastAPI()
@@ -26,29 +25,4 @@ app.add_middleware(
 
 app.include_router(user_routes, prefix='/user')
 app.include_router(stock_routes, prefix='/stocks')
-
-from data.socket_pool import SocketPool
-import asyncio
-from typing import Dict
-NEWS_POOL = SocketPool()
-
-@app.websocket('/news/')
-async def connect_websocket(websocket: WebSocket):
-    try:
-        await websocket.accept()
-        NEWS_POOL.add(websocket)
-        while True:
-            await websocket.receive_text()
-
-    except WebSocketDisconnect:
-        NEWS_POOL.remove(websocket)
-
-from middleware import check_admin
-
-@app.post('/news/')
-def broadcast_news(
-    data: Dict[str, str],
-    _: None = Depends(check_admin),
-):
-    asyncio.run(NEWS_POOL.broadcast(data))
-    return {"detail": "News broadcasted"}
+app.include_router(misc_routes)
---
+++ server/misc_views.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
+from data import db
+from data.cache import Cache
+import middleware
+
+import stock.models as stock_models
+import user.models as user_models
+
+router = APIRouter()
+
+@router.get('/leaderboard')
+def get_leaderboard(session: db.sql.Session = Depends(db.get_session)):
+    res = {}
+    for user, holding in session.exec(
+        db.sql.select(user_models.User, user_models.Holding)
+        .join(user_models.Holding)
+        .where(user_models.User.verified == True)
+    ).all():
+        if not res.get(user.username, False): res[user.username] = user.balance
+        cache_entry = Cache().get(holding.stock.hex)
+        res[user.username] += holding.quantity * \
+            (stock_models.StockEntry.from_json(holding.stock, cache_entry).close if cache_entry else 0)
+
+    return res
+
+
+
+
+from data.socket_pool import SocketPool
+import asyncio
+from typing import Dict
+NEWS_POOL = SocketPool()
+
+@router.websocket('/news/')
+async def connect_websocket(websocket: WebSocket):
+    try:
+        await websocket.accept()
+        NEWS_POOL.add(websocket)
+        while True:
+            await websocket.receive_text()
+
+    except WebSocketDisconnect:
+        NEWS_POOL.remove(websocket)
+        
+
+@router.post('/news/')
+def broadcast_news(
+    data: Dict[str, str],
+    _: None = Depends(middleware.check_admin),
+):
+    asyncio.run(NEWS_POOL.broadcast(data))
+    return {"detail": "News broadcasted"}
